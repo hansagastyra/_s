6761f731f7debe278cd9beb4eaecfbe4b670fa2b
Hans Agastyra <hans.agastyra@gmail.com>
2014-04-20 20:27:57 +0700

    Add Foundation, sassy_s, and FontAwesome.

--- functions.php
+++ functions.php
@@ -87,11 +87,28 @@ add_action( 'widgets_init', '_s_widgets_init' );
  * Enqueue scripts and styles.
  */
 function _s_scripts() {
+	//-reset using normalize.css
+	wp_enqueue_style('normalize', get_stylesheet_directory_uri().'/css/normalize.css');
+	//-foundation
+	wp_enqueue_style('foundation', get_stylesheet_directory_uri().'/css/foundation.min.css');
+	//-font-awesome
+	wp_enqueue_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css');
+	//-main style
 	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
+	//-modernizr (on head)
+	wp_enqueue_script('modernizr', get_stylesheet_directory_uri().'/js/foundation/vendor/modernizr.js', array(), false, false);
+	//-jquery (on footer)
+	wp_deregister_script('jquery');
+	wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', true);
+	wp_enqueue_script('jquery');
+	//-foundation js
+	wp_enqueue_script('foundation', get_stylesheet_directory_uri().'/js/foundation/foundation.min.js', array('jquery'), '5.2.2', true);
+	
+	//Our main scripts
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( '_s-script', get_template_directory_uri() . '/js/main.js', array(), false, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -99,10 +116,11 @@ function _s_scripts() {
 }
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
 
+
 /**
  * Implement the Custom Header feature.
  */
-//require get_template_directory() . '/inc/custom-header.php';
+require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
