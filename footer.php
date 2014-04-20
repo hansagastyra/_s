0c49a89e3d85c396b4e53c097ec511cbb618f27a
Hans Agastyra <hans.agastyra@gmail.com>
2014-04-20 21:40:34 +0700

    change the footer default

--- footer.php
+++ footer.php
@@ -9,12 +9,14 @@
 ?>
 
 	</div><!-- #content -->
-
+	
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', '_s' ) ); ?>"><?php printf( __( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<span class="sep"> x </span>
+			<?php $themeinfo = wp_get_theme(); ?>
+			<?php printf( __( '%1$s', '_s' ), 
+				'<a href="' . $themeinfo->get('ThemeURI') . '" rel="theme-name">' . $themeinfo->get('Name') . '</a>'); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
